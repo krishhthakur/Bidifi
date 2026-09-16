d9bbd79f5104a9a70cbc105fa0c7f28d11fd8c27
Krish <krishhthakur99@gmail.com>
2026-09-17 03:08:05 +0530

    diagnose vercel runtime

---
+++ api/runtime-test.js
@@ -0,0 +1,9 @@
+﻿module.exports = function handler(req, res) {
+  res.status(200).json({
+    node: process.version,
+    platform: process.platform,
+    arch: process.arch,
+    cwd: process.cwd(),
+    type: typeof require
+  });
+};
